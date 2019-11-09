f79abef6e0c5ec95cbcc7384db55a1496815c83b
olayimika <adejumoolayimika@gmail.com>
2019-11-09 23:25:17 +0100

    Part 3 Corrected Parts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,7 +13,7 @@ export class ProductService {
 
   }
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => 
+    return this._http.get(this._albumUrl).map((response) =>
     response.json());
   }
 
